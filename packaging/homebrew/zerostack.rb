c0ffec33b329681bb94e4b83a4c49bf9d974bd36
Giuseppe <djtech.developer@gmail.com>
2026-07-01 00:43:18 +0200

    bump to v1.6.0 as a rename from v1.5.1

--- packaging/homebrew/zerostack.rb
+++ packaging/homebrew/zerostack.rb
@@ -1,26 +1,26 @@
 class Zerostack < Formula
   desc "Minimalistic coding agent written in Rust, optimized for memory footprint and performance"
   homepage "https://github.com/gi-dellav/zerostack"
-  version "1.5.1"
+  version "1.6.0"
   license "GPL-3.0-only"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/gi-dellav/zerostack/releases/download/v1.5.1/zerostack-x86_64-apple-darwin.tar.gz"
-      sha256 "0c5ce2d6cc251bb6dd782f250a321bbb13084d0e88a9536d9d86d7b04a5779e0"
+      url "https://github.com/gi-dellav/zerostack/releases/download/v1.6.0/zerostack-x86_64-apple-darwin.tar.gz"
+      sha256 "fdd814adeb5988f40106eaed7720c53bd4e0d2cf26d9b0b48e51cda573a6faa1"
     else
-      url "https://github.com/gi-dellav/zerostack/releases/download/v1.5.1/zerostack-aarch64-apple-darwin.tar.gz"
-      sha256 "100c1a7182343d916e126b342e3cc32bf12bf44e0fa17e392d441171b31c3ebc"
+      url "https://github.com/gi-dellav/zerostack/releases/download/v1.6.0/zerostack-aarch64-apple-darwin.tar.gz"
+      sha256 "196595df1b9e3ad19fe54ce891606634ab0fab4e900d12e92078642471e77fb9"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/gi-dellav/zerostack/releases/download/v1.5.1/zerostack-x86_64-unknown-linux-musl.tar.gz"
-      sha256 "b8b35c4afdc5866ec137e1c15d3dfe47382bc05be17c1360fff1c0382a912aff"
+      url "https://github.com/gi-dellav/zerostack/releases/download/v1.6.0/zerostack-x86_64-unknown-linux-musl.tar.gz"
+      sha256 "854232371f44f1493c9b5f0046656a9c4f1a9376b2c7a050f5555657708d5ec3"
     else
-      url "https://github.com/gi-dellav/zerostack/releases/download/v1.5.1/zerostack-aarch64-unknown-linux-musl.tar.gz"
-      sha256 "0d81ceef899f2a8be800857dca6165fbf3bee682ba12f0f9ccd291abaca7ec0c"
+      url "https://github.com/gi-dellav/zerostack/releases/download/v1.6.0/zerostack-aarch64-unknown-linux-musl.tar.gz"
+      sha256 "d23fd7e51d582773a7cae4804767c547edcc69ea04e3f545d53210811b474237"
     end
   end
 
